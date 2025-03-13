6a6057b22d902628f88b3c38fb792bd72a31f20a
tom2004ber <tommiberto04@gmail.com>
2025-03-13 20:24:27 +0100

    Aggiunto file prova.py e esercizi nel file txt

---
+++ prova.py
@@ -0,0 +1,10 @@
+
+while(True):
+    n = int(input("Chiedi numero: "))
+    if(n > 5):
+        print("numero maggiore di 5")
+    elif(n == 5):
+        print("numero uguale a 5")
+    else:
+        print("numero minore di 5")
+
